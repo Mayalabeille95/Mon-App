9ea89d75699270df1bfab82e586e7d206787bc2c
Ahmed_Hmed95 <seghiriahmed9@gmail.com>
2024-11-25 13:51:36 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
+import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cours from "./Pages/Cours";
+import Contact from "./Pages/Contact";
+import Connexion from "./Pages/Connexion";
+import Inscription from "./Pages/Inscription";
+import NotFound from "./Pages/NotFound";
+import Abonnement from "./Pages/Abonnenemnt";
+import MotDePasseOublié from "./Pages/Mot de passe oublié";
 
 function App() {
   return (
     <div className="App">
-      <h1>blablabla</h1>
-      <h1>blablabla</h1>
-      <h1>blablabla</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Cours" element={<Cours />} />
+          <Route path="/Abonnement" element={<Abonnement />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Connexion" element={<Connexion />} />
+          <Route path="/Inscription" element={<Inscription />} />
+          <Route path="/Mot de Passe oublié" element={<MotDePasseOublié />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
