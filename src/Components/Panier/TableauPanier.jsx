f37bf3fd8a13b65085d9f3b1224869b489bcb753
Ahmed_Hmed95 <seghiriahmed9@gmail.com>
2024-11-26 15:30:19 +0100

    Tableau Panier modified

--- src/Components/Panier/TableauPanier.jsx
+++ src/Components/Panier/TableauPanier.jsx
@@ -2,32 +2,31 @@ import React from "react";
 
 const TableauPanier = ({ items, total }) => {
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Abonnement</th>
-            <th>Durée</th>
-            <th>Prix</th>
+    <table>
+      <thead>
+        <tr>
+          <th>Abonnement</th>
+          <th>Durée</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.duration}</td>
+            <td>{item.price.toFixed(2)} $</td>
           </tr>
-        </thead>
-        <tbody>
-          {items.map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.duration}</td>
-              <td>{item.price.toFixed(2)} $</td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="2">Total</td>
-            <td>{total} $</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan="2">Total</td>
+          {/* Appel de la fonction `total()` pour recalculer si nécessaire */}
+          <td>{total()} $</td>
+        </tr>
+      </tfoot>
+    </table>
   );
 };
 
