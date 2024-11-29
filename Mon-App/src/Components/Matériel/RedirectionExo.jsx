fe1b8e5acb50dd20f926294de8c3640865b24002
Mayalabeille95 <Seghiriahmed9@gmail.com>
2024-11-29 16:05:10 +0100

    RedirectionExo update

--- Mon-App/src/Components/Matériel/RedirectionExo.jsx
+++ Mon-App/src/Components/Matériel/RedirectionExo.jsx
@@ -2,35 +2,35 @@ import React from "react";
 
 const RedirectionExo = () => {
   return (
-    <nav className="redirection-container" id="redirection-exo">
+    <nav className="redirection-container">
       <ul className="redirection-list">
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#Epaulesdos" className="redirection-link">
             Epaule Dos
           </a>
         </li>
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#Bras" className="redirection-link">
             Bras
           </a>
         </li>
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#PecsAbdos" className="redirection-link">
             Pecs Abdos
           </a>
         </li>
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#Jambes" className="redirection-link">
             Jambes
           </a>
         </li>
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#Fessiers" className="redirection-link">
             Fessiers
           </a>
         </li>
         <li className="redirection-item">
-          <a href="../Pages/VideoMatériel.jsx" className="redirection-link">
+          <a href="#Cardio" className="redirection-link">
             Cardio
           </a>
         </li>
